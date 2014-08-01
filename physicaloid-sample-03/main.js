552b6cc411cbfe97ed5ca768ffd54d96cd19f6e5
José Juan Sánchez <josejuan.sanchez@gmail.com>
2014-08-01 02:57:43 +0200

    Update main.js

--- physicaloid-sample-03/main.js
+++ physicaloid-sample-03/main.js
@@ -6,7 +6,7 @@
 
 var arduino = boards.startArduino();
 
-// Lists all the supported devices where you can use the "upload" function
+// Lists all the supported devices that you can use with the "upload" function
 var list = arduino.getSupportedDevices();
 list.forEach(function(device) {
     console.log(device);
